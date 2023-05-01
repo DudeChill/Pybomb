279ef8b2b680262e42d7bfdb9bb0d9b7c58759de
Dude_Just_Game <34346339+DudeChill@users.noreply.github.com>
2023-05-01 15:00:46 -0700

    Update Bomberman.py

--- Bomberman.py
+++ Bomberman.py
@@ -6,10 +6,11 @@ height = 11
 map = []
 x = 0
 y = 0
+
 for i in range(height):
     map.append([])
     for u in range(width):
-        if(i > 0 and i < height and u > 0 and u < width and i % 2 == 0 and u  % 2 == 0):
+        if(i > 0 and i < height and u > 0 and u < width and i % 2 == 1 and u  % 2 == 1):
             map[i].append("#")
         else:
             map[i].append("_")
@@ -27,20 +28,35 @@ def screen():
 map[x][y] = player
 screen()
 
-def right():
-    x+=1
-    map[x][y] = player
+def movePlayer(map,player,direction):
+    coords = []
+    g = j
+    h = k
+    for j in range(len(map)):
+        for k in range(leng(map[j])):
+            if(map[j][k] == "@"):
+                x = j
+                y = k
+    if(direction == "right"): 
+        g+=1
+    elif(direction == "left"): 
+        g-=1
+    elif(direction == "up"): 
+        h-=1
+    elif(direction == "down"): 
+        h+=1
+    map[x][y], map[g][h] = map[g][h], map[x][y]
     screen()
 
 def on_key_release(key):
     if key == Key.right: 
-        right()
+        movePlayer(map,player,"right")
     elif key == Key.left:
-        print("Left key clicked")
+        movePlayer(map,player,"left")
     elif key == Key.up:
-        print("Up key clicked")
+        movePlayer(map,player,"up")
     elif key == Key.down:
-        print("Down key clicked")
+        movePlayer(map,player,"down")
     elif key == Key.esc:
         exit()
 with keyboard.Listener(on_release=on_key_release) as listener:
